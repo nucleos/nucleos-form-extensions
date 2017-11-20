25b7a300eb16e11923bde2bc05c8417e27fe068f
core23 <mail@core23.de>
2017-11-20 18:17:56 +0100

    Removed compiler pass

--- src/Core23FormExtensionsBundle.php
+++ src/Core23FormExtensionsBundle.php
@@ -9,17 +9,8 @@
 
 namespace Core23\FormExtensionsBundle;
 
-use Core23\FormExtensionsBundle\DependencyInjection\Compiler\FormCompilerPass;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class Core23FormExtensionsBundle extends Bundle
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function build(ContainerBuilder $container)
-    {
-        $container->addCompilerPass(new FormCompilerPass());
-    }
 }
