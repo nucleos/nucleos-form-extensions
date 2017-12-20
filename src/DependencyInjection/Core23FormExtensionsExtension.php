5f9c92a8ea937f7f74954c0f2cc1604cfd12eb2a
core23 <mail@core23.de>
2017-12-20 18:55:38 +0100

    Always register form themes to twig

--- src/DependencyInjection/Core23FormExtensionsExtension.php
+++ src/DependencyInjection/Core23FormExtensionsExtension.php
@@ -26,5 +26,35 @@ final class Core23FormExtensionsExtension extends Extension
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('form.xml');
         $loader->load('validator.xml');
+
+        $this->registerWidget($container);
+    }
+
+    /**
+     * Registers the form widget.
+     *
+     * @param ContainerBuilder $container
+     */
+    private function registerWidget(ContainerBuilder $container): void
+    {
+        $templatingEngines = $container->getParameter('templating.engines');
+
+        if (in_array('php', $templatingEngines)) {
+            $formRessource = 'Core23FormExtensionsBundle:Form';
+
+            $container->setParameter('templating.helper.form.resources', array_merge(
+                $container->getParameter('templating.helper.form.resources'),
+                [$formRessource]
+            ));
+        }
+
+        if (in_array('twig', $templatingEngines)) {
+            $formRessource = 'Core23FormExtensionsBundle:Form:widgets.html.twig';
+
+            $container->setParameter('twig.form.resources', array_merge(
+                $container->getParameter('twig.form.resources'),
+                [$formRessource]
+            ));
+        }
     }
 }
