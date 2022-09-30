839774f33977666b4081efd6c29e0734aa61f1f3
core23 <mail@core23.de>
2022-09-30 20:16:32 +0200

    Fix tests

---
+++ tests/Fixtures/FooEntity.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * (c) Christian Gripp <mail@core23.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nucleos\Form\Tests\Fixtures;
+
+class FooEntity
+{
+}
